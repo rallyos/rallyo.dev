5f2c0c9828891eb813cc330be23df2f337d1c1b2
Dimitar Ralev <dim.ralev@gmail.com>
2019-02-23 14:11:02 +0200

    Publish

--- js/bridge.js
+++ js/bridge.js
@@ -3,21 +3,34 @@ document.addEventListener("DOMContentLoaded", function(){
 
     function setTimeOfDay(t) {
         document.querySelector('.wrapper').className = 'wrapper ' + t;
-        localStorage.setItem('dayOrNight', t)
+        localStorage.setItem('timeOfDay', t)
     }
 
-    if (localStorage.getItem('dayOrNight')) {
-        var dayOrNight = localStorage.getItem('dayOrNight');
-        setTimeOfDay(dayOrNight)
+    function getTimeOfDay() {
+        var d = new Date()
+
+        if (d.getHours() > 19 && d.getHours() < 7) {
+            return 'night';
+        }
+
+        return 'day';
+    }
+
+
+
+    if (localStorage.getItem('timeOfDay')) {
+        setTimeOfDay(localStorage.getItem('timeOfDay'));
     } else {
-        localStorage.setItem('dayOrNight', 'day')
-        setTimeOfDay('day')
+        setTimeOfDay(getTimeOfDay());
     }
 
     circles.forEach(function(circle) {
         circle.addEventListener('click', function() {
             var dayOrNight = this.dataset.time;
-            setTimeOfDay(dayOrNight)
+            setTimeOfDay(dayOrNight);
         });
     })
+
+
+
 });
